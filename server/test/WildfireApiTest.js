357cdae7044abe508311d14d7dc993f9431419d0
nikhildhirmalani <nk273610@dal.ca>
2022-11-13 15:40:00 -0500

    changed the tests
    change the /api path

--- server/test/WildfireApiTest.js
+++ server/test/WildfireApiTest.js
@@ -4,7 +4,7 @@ const { chai, server, should } = require("./testConfig");
 describe("/GET get the wildfire data API test", () => {
     it("It should send validation error for Wildfire API as no query param is present", (done) => {
         chai.request(server)
-            .get("/wildfires/ended")
+            .get("/api/wildfires/ended")
             .end((err, res) => {
                 res.should.have.status(400);
                 res.body.should.have.property("message").eql("The month parameter cannot be empty");
@@ -15,7 +15,7 @@ describe("/GET get the wildfire data API test", () => {
 
     it("It should send validation error for Wildfire API as incorrect query param is present", (done) => {
         chai.request(server)
-            .get("/wildfires/ended?month=ma&year=2020")
+            .get("/api/wildfires/ended?month=ma&year=2020")
             .end((err, res) => {
                 res.should.have.status(400);
                 res.body.should.have.property("message").eql("The month parameter is invalid");
@@ -25,7 +25,7 @@ describe("/GET get the wildfire data API test", () => {
 
     it("It should send validation error for Wildfire API as incorrect query param is present", (done) => {
         chai.request(server)
-            .get("/wildfires/ended?month=mar")
+            .get("/api/wildfires/ended?month=mar")
             .end((err, res) => {
                 res.should.have.status(400);
                 res.body.should.have.property("message").eql("The year parameter cannot be empty");
@@ -35,7 +35,7 @@ describe("/GET get the wildfire data API test", () => {
 
     it("It should send validation error for Wildfire API as incorrect query param is present", (done) => {
         chai.request(server)
-            .get("/wildfires/ended?month=mar&year=202")
+            .get("/api/wildfires/ended?month=mar&year=202")
             .end((err, res) => {
                 res.should.have.status(400);
                 res.body.should.have.property("message").eql("The year parameter is invalid");
@@ -45,7 +45,7 @@ describe("/GET get the wildfire data API test", () => {
 
     it("It should send validation error for Wildfire API as incorrect query param is present", (done) => {
         chai.request(server)
-            .get("/wildfires/ended?month=mar&year=20224")
+            .get("/api/wildfires/ended?month=mar&year=20224")
             .end((err, res) => {
                 res.should.have.status(400);
                 res.body.should.have.property("message").eql("The year parameter is invalid");
